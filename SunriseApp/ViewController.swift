a6f063aa7ee14b36553e3ec56e7ff275930f0a19
BDankovych <bohdan.dankovych@gmail.com>
2018-11-05 23:33:00 +0200

    init commit

---
+++ SunriseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunriseApp
+//
+//  Created by Bohdan Dankovych on 11/5/18.
+//  Copyright © 2018 Bohdan Dankovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
